0288e8089ca61b1f8249b2901735de920055b60d
Grayce Muthui <graycemuthui20@gmail.com>
2022-09-28 17:11:55 +0300

    Logging out users

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,6 @@
 class MainController < ApplicationController
   def index
-  # look for a user with the id in the session
-    if session[:user_id]
-    @user = User.find_by(id : session[:user_id])
+    # look for a user with the id in the session
+    @user = User.find_by(id: session[:user_id]) if session[:user_id]
   end
 end
