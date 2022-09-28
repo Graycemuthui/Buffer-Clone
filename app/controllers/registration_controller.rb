af7a9293f88eb1c574ca236a9680d449ec1957eb
Grayce Muthui <graycemuthui20@gmail.com>
2022-09-28 16:13:07 +0300

    Handling errors

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -1,6 +1,22 @@
 class RegistrationController < ApplicationController
-# we call the user with the @ so it can be accessible in both views and controllers
- def new
-  @user = User.new
- end
-end
+  # we call the user with the @ so it can be accessible in both views and controllers
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: 'You have successfully signed up!'
+    else
+      render :new
+      end
+    end
+
+    private
+# permit specific inputs from the user
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+  end
+
