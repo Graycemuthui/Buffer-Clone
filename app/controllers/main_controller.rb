876afa82cdff1a6c37f7939b28b3e4a3b5bf5f1e
Grayce Muthui <graycemuthui20@gmail.com>
2022-09-28 16:36:52 +0300

    Login with session cookies

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,3 +1,7 @@
 class MainController < ApplicationController
-  def index; end
+  def index
+  # look for a user with the id in the session
+    if session[:user_id]
+    @user = User.find_by(id : session[:user_id])
+  end
 end
--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -7,16 +7,19 @@ class RegistrationController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      # this signs in the user
+      session[:user_id] = @user.id
+      # sends back to the homepage
       redirect_to root_path, notice: 'You have successfully signed up!'
     else
       render :new
-      end
-    end
-
-    private
-# permit specific inputs from the user
-    def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation)
     end
   end
 
+  private
+
+  # permit specific inputs from the user
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
